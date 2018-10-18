1dc2bf17b8562c0322e4439b02ecc6c6a25b3ed6
heather sterman <heather.sterman531@gmail.com>
2018-10-18 21:32:03 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,3 +1,8 @@
 artists = ["Frida Kahlo", "Pablo Picasso", "Cai Guo-Qiang"]
 artists.pop
 end
+
+singers = ["Hozier", "Ariana Grande", "Usher"]
+next_singer = "Beyonce"
+singers.push(next_singer)
+end 
